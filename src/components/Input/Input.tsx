8dd68cbe07e2c1027e4bb06e3fad30f9ff3bc1f2
Igor_Goryany <Mr.Q.U.B.E31@gmail.com>
2025-01-28 02:25:34 +0300

    add label to input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,19 @@
-import { ComponentProps } from "react";
+import { ComponentProps, useId } from "react";
 import s from "./Input.module.css";
 interface InputProps extends ComponentProps<"input"> {
   icon?: React.ReactNode;
+  label?: string;
 }
 export const Input = (props: InputProps) => {
-  const { className, icon, ...rest } = props;
+  const { className, icon, label, ...rest } = props;
+  const id = useId();
   return (
-    <div className={`${s.input} ${className}`}>
-      {icon && <div className={s.icon}>{icon}</div>}
-      <input type="text" {...rest} />
+    <div className={`${s.container} ${className}`}>
+      {label && <label htmlFor={id}>{label}</label>}
+      <div className={s.input}>
+        {icon && <div className={s.icon}>{icon}</div>}
+        <input type="text" {...rest} />
+      </div>
     </div>
   );
 };
